cb3048c754a8a0eac55006bcfdcf718f2fc27be5
Jesse Martinez <mjesse@gmail.com>
2015-12-14 20:29:20 -0500

    adding NAGPRA claim config

--- services/JaxRsServiceProvider/src/main/java/org/collectionspace/services/jaxrs/CollectionSpaceJaxRsApplication.java
+++ services/JaxRsServiceProvider/src/main/java/org/collectionspace/services/jaxrs/CollectionSpaceJaxRsApplication.java
@@ -53,6 +53,7 @@ import org.collectionspace.services.person.PersonAuthorityResource;
 import org.collectionspace.services.citation.CitationAuthorityResource;
 import org.collectionspace.services.exhibition.ExhibitionResource;
 import org.collectionspace.services.conditioncheck.ConditioncheckResource;
+import org.collectionspace.services.claim.ClaimResource;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.core.Application;
@@ -122,6 +123,7 @@ public class CollectionSpaceJaxRsApplication extends Application
         addResourceToMapAndSingletons(new ConditioncheckResource());
         addResourceToMapAndSingletons(new ValuationcontrolResource());
         addResourceToMapAndSingletons(new ObjectExitResource());
+        addResourceToMapAndSingletons(new ClaimResource());
         addResourceToMapAndSingletons(new BatchResource());
         addResourceToMapAndSingletons(new MediaResource());
         addResourceToMapAndSingletons(new BlobResource());
